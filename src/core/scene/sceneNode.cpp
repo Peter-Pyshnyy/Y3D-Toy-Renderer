f50ad8839816ed91c957c6aa117641e1647658cd
Peter-Pyshnyy <pyshnyy.peter@gmail.com>
2025-11-10 22:05:07 +0100

    Implement SceneNode child management

--- src/core/scene/sceneNode.cpp
+++ src/core/scene/sceneNode.cpp
@@ -5,11 +5,44 @@ SceneNode::SceneNode(const std::string& name)
 {
 }
 
-SceneNode::~SceneNode()
-{
-	for (SceneNode* child : children)
-	{
+SceneNode::~SceneNode() {
+	for (SceneNode* child : children) {
 		delete child;
 	}
 	parent = nullptr;
 }
+
+void SceneNode::addChild(std::unique_ptr<SceneNode> child) {
+    if (child) {
+        child->parent = this;
+        children.emplace_back(std::move(child));
+    }
+}
+
+std::unique_ptr<SceneNode> SceneNode::removeChild(SceneNode* child) {
+    auto found = std::find_if(children.begin(), children.end(),
+        [child](const std::unique_ptr<SceneNode>& p) { return p.get() == child; });
+
+    if (found == children.end())
+        throw std::runtime_error("Child not found in SceneNode::removeChild");
+
+	std::unique_ptr<SceneNode> result = std::move(*found); // found is an iterator to unique_ptr, thus *found is the unique_ptr
+    children.erase(found);
+    result->parent = nullptr; // if you track parent pointers
+    return result; // ownership transferred to caller
+}
+
+int SceneNode::getChildCount() const {
+    return static_cast<int>(children.size());
+}
+
+SceneNode* SceneNode::getChild(int index) const {
+        if (index < 0 || index >= static_cast<int>(children.size()))
+            throw std::out_of_range("Index out of range in SceneNode::getChild");
+		return children[index].get();
+}
+
+SceneNode* SceneNode::getParent() const {
+    return parent;
+}
+
--- src/core/scene/sceneNode.h
+++ src/core/scene/sceneNode.h
@@ -9,21 +9,24 @@ public:
 		SceneNode(const std::string& name);
 		virtual ~SceneNode();
 
+		// TODO: add rule of 5 AND empty virtual draw() method
+
 		void addChild(std::unique_ptr<SceneNode> child);
-		void removeChild(SceneNode* child);
+		std::unique_ptr<SceneNode> removeChild(SceneNode* child);
 		int getChildCount() const;
 		SceneNode* getChild(int index) const;
 		SceneNode* getParent() const;
 
-		void setLocalTransform(const glm::vec3& position, const glm::vec3& rotation, const glm::vec3& scale);
-		void setLocalTransform(const glm::mat4& matrix);
+		void setModelMatrix(const glm::mat4& matrix);
 		void translate(const glm::vec3& delta);
 		void rotate(const glm::vec3& euler);
 		void scale(const glm::vec3& factor);
 		glm::mat4 getLocalTransform() const;
 		glm::mat4 getWorldTransform() const;
+
+		void draw() const;
 protected:
-	glm::mat4 localTransform;
+	glm::mat4 modelMatrix;
 	SceneNode* parent;
 	std::vector<std::unique_ptr<SceneNode>> children;
 };
